9c304170038b653b9e9fd58710fbc278036788f0
sdellis <shaun@sdellis.com>
2013-04-21 23:33:30 +0100

    Lambertville Suitcase Drum started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lsd::Application.config.secret_token = '73e0814d62db4b9a9c8d7af0d11a96a7c00708d59ff1dc9f23316b6c1be43f8c258e1f5e2291dbb9cf97800a29ee7207bacc2b89068485b642d8a241f7368dd5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lsd::Application.config.session_store :cookie_store, key: '_lsd_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lsd::Application.config.session_store :active_record_store
