fe307489e2e3c6c89c971555f08a21d62b4dc58b
sdellis <shaun@sdellis.com>
2013-05-05 21:28:22 +0100

    added tests

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -24,6 +24,16 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_redirected_to show_path(products(:acme).id)
   end
 
+  test "should create line_item via ajax" do 
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:acme).id
+    end
+    assert_response :success
+    assert_select_jquery :html, '#shopcart' do
+      assert_select 'tr#current_item td', /MyString/
+    end
+  end
+
   test "should show line_item" do
     get :show, id: @line_item
     assert_response :success
